518737ab0343c26625ac662af76b629975daed3b
Ali Pourgheysari <a.geysar@gmail.com>
2023-06-07 00:07:17 +0330

    added random generation

--- Code/main.py
+++ Code/main.py
@@ -73,7 +73,7 @@ labels = np.array([0 if y == 'legitimate' else 1 for y in labels] )
 ## reducing dimension
 records_dim_reduced = feature_extraction(records_vectorized)
 
-records_dim_reduced[:5]
+# records_dim_reduced[:5]
 
 records_vectorized = pd.DataFrame(records_vectorized, columns=feature_names)
 
@@ -83,3 +83,101 @@ records_selection, feature_name_selection = feature_selection(records_vectorized
 pd.DataFrame(records_selection, columns=feature_name_selection).head()
 
 ## TODO: build a fuzzy rule-based model for (records, label) classification
+
+# Define the Rule Base 
+# Define linguistic variables and rules here
+class Fuzzy_sets:
+    # def max_min_of_features():
+    #     maximum = records_dim_reduced[0][0]
+    #     minimum = records_dim_reduced[0][0]
+
+    #     for sublist in records_dim_reduced:
+    #         for element in sublist:
+    #             if element > maximum:
+    #                 maximum = element
+    #             if element < minimum:
+    #                 minimum = element
+        
+    #     return minimum, maximum
+
+    def sigmoid(x, s, m):
+        return 1 / (1 + np.exp(-(x-m)/s))
+        
+
+    def gaussian(x, s, m):
+        return np.exp((-1/2)*((x-m)/s)**2)
+
+
+    def trapezius(x, s, m):
+        return max(min((x-m)/s, 1), 0)
+
+
+    def triangular(x, s, m):
+        return max(min((x-m)/s, (m-x)/s), 0)
+
+
+    # def generate_rule(self):
+    #     # [5, -6.3, 9.8, 0.1, 99]
+    #     # [[[low, s, m, sigmoid], [high, 9, 2, sigmoid], [medium, 5, 4, sigmoid], [low, 5, 4, sigmoid], [low, 5, 4], [high, 5, 4]]
+    #     #  [[high, 5, 4], [medium, 5, 4], [low, 5, 4], [, 5, 4], [low, 5, 4], [high, 5, 4]]]]
+    #     population = []
+
+    #     minimum, maximum = records_dim_reduced.max, records_dim_reduced.min
+    #     for i in range(50):
+    #         rule = []
+    #         for j in range(5):
+    #             term = []
+    #             s = 0
+                
+    #             term.append(np.random.choice(['sigmoid', 'gaussian', 'triangular', 'trapezius'])) # membership function
+    #             term.append(np.random.randint(minimum, maximum)) # m      ########## we must calculate the min and max of each feature
+    #             while s == 0:
+    #                 term.append(np.random.randint(1, 100) if term[0] == 'triangular' else np.random.randint(-100, 100)) # s
+    #             term.append(np.random.choice(['low', 'fairly low', 'medium', 'fairly high', 'high']))
+            
+    #             rule.append(term)
+
+    #         population.append(rule)
+
+    #     return population
+    
+class Rule:
+    def __init__(self, population=50) -> None:
+        self.if_term = self.generate_if_term(population)
+        self.class_label = self.generate_class_label(population)
+        self.fitness = None
+        self.is_calculated = False
+    
+    def generate_if_term(population):
+        minimum, maximum = records_dim_reduced.max, records_dim_reduced.min
+        rule = []
+        for j in range(5):
+            term = []
+            s = 0
+            
+            term.append(np.random.choice(['sigmoid', 'gaussian', 'triangular', 'trapezius'])) # membership function
+            term.append(np.random.randint(minimum, maximum)) # m      ########## we must calculate the min and max of each feature
+            while s == 0:
+                term.append(np.random.randint(1, 100) if term[0] == 'triangular' else np.random.randint(-100, 100)) # s
+            term.append(np.random.choice(['low', 'fairly low', 'medium', 'fairly high', 'high']))
+        
+            rule.append(term)
+        return rule
+    
+    def generate_class_label(population):
+        return np.random.randint(0, 1)
+
+
+
+def genetic_algorithm():
+    
+    population = Rule(50)
+
+
+
+def cross_over():
+    pass
+def mutation():
+    pass
+def fitness():
+    pass
